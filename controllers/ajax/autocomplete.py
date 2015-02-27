6abcbf83911822da7a01c2f6a2d554bb621535f7
Jonathan Lee <123abcjonathanlee@gmail.com>
2015-02-26 20:07:15 -0800

    git ignore added

--- controllers/ajax/autocomplete.py
+++ controllers/ajax/autocomplete.py
@@ -1,5 +1,8 @@
 import webapp2
 import json
+import urllib
+AUTOCOMPLETE_URL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?"
+API_KEY = "AIzaSyCnbz5OlqvlGaThq9b115HEalV-8asu9CY"
 class AutocompletePage(webapp2.RequestHandler):
     def get(self):
         self.redirect("/")
@@ -7,4 +10,31 @@ class AutocompletePage(webapp2.RequestHandler):
         req = json.loads(self.request.body)                     #raw request
         q = req['q']                                            #query term
 
-        self.response.out.write(q)
+        params = {"input": q,
+                  "types": "establishment",
+                  "key": API_KEY}
+        url = AUTOCOMPLETE_URL + urllib.urlencode(params)
+
+        response = urllib.urlopen(url)
+        data = json.loads(response.read())
+        if(data['status'] != 'OK' or len(data['predictions']) == 0):
+            results = {"status": "ERROR"}
+        else:
+            apiPredictions = data['predictions']
+            results = {"status": "OK",
+                       "predictions": []}
+            for apiPrediction in apiPredictions:
+                description = apiPrediction['description']
+                location = ""
+                endIndex = description.find(",")
+
+                if(endIndex < 0):
+                    title = description
+                else:
+                    title = description[0:endIndex]
+                    location = description[endIndex  + 1:len(description)]
+
+                results['predictions'].append({"title": title, "location": location})
+
+
+        self.response.out.write(json.dumps(results))
--- views/js/home.js
+++ views/js/home.js
@@ -1,14 +1,45 @@
-$('#searchBox input').keypress(function(){
-    $.ajax({
-        type:"post",
-        url:"/ajax/autocomplete/",
-        dataType:"text",
-        data: JSON.stringify({"q": $(this).val()})
-    }).done(function(data){
-	    //var lis = $('#autocomplete li');
-	    //console.log(lis[0].text());
-        console.log("post response: " + data)
-    });
+var keyState = 0;
+var lastValue = $('#searchBox input').val();
+$('#searchBox input').keydown(function(){
+    keyState = 1;
+});
+
+
+
+
+$('#searchBox input').keyup(function(){
+    var val = $(this).val();
+    if(keyState == 1 && lastValue != val)
+    {
+        $.ajax({
+            type:"post",
+            url:"/ajax/autocomplete/",
+            dataType:"json",
+            data: JSON.stringify({"q": $(this).val()})
+        }).done(function(data){
+            //var lis = $('#autocomplete li');
+            //console.log(lis[0].text());
+            console.log("post response: " + data["status"]);
+            $('#autoBox').html("");
+            if(data["status"] === "OK")
+            {
+                console.log("go bro");
+                for(var i = 0; i < data["predictions"].length; i++)
+                {
+                    var title = data["predictions"][i]["title"];
+                    var location = data["predictions"][i]["location"];
+                    html = "<li><b>" + title + "</b> " + location + "</li>";
+                    $('#autoBox').append(html);
+                }
+            }
+            else
+            {
+                console.log("no go bro");
+            }
+        });
+    }
+    lastValue = val;
+    keyState = 0;
 });
 
 $('#searchBox button').click(function(){
